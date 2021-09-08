6e8c872ad0b218e1d27f7ed0ff686a5c140d5add
MOhamed ELfeky <mohamedelfeky202020@gmail.com>
2021-09-08 09:34:03 +0200

    Adding grid

--- Sudoku solver with Backtracking.py
+++ Sudoku solver with Backtracking.py
@@ -1,12 +1,29 @@
 import pygame
 
+#window parameters
 Width = 550
-back_ground_color = (0,0,0)
+
+#colors
+back_ground_color = (255,255,255)
+black = (0,0,0)
 
 def main():
   pygame.init()
   win = pygame.display.set_mode((Width,Width))
   pygame.display.set_caption("Sudoku")
+  win.fill(back_ground_color)
+
+  #adding grid
+  for i in range(0,10):
+      if i%3 == 0:
+          pygame.draw.line(win,black,(50+50*i,50),(50+50*i,500),4) #Bold lines
+          pygame.draw.line(win,black,(50,50+50*i),(500,50+50*i),4)
+          
+      pygame.draw.line(win,black,(50+50*i,50),(50+50*i,500),2)
+      pygame.draw.line(win,black,(50,50+50*i),(500,50+50*i),2)
+
+  pygame.display.update()  
+
 
   while True:
     for event in pygame.event.get():
